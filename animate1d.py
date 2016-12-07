96f19503309ec782dec610d8ac40a3bff45e5f71
Anders Johansson <anjohan@uio.no>
2016-12-07 20:32:26 +0100

    Laget animering

---
+++ animate1d.py
@@ -0,0 +1,27 @@
+from matplotlib.pyplot import *
+import sys
+import os
+import re as regex
+
+name = sys.argv[1][:-4]
+
+with open(name + ".dat","r") as infile:
+    lines = infile.readlines()
+
+x = [float(val) for val in lines[0].split()[1:]]
+i = 0
+for line in lines[1:]:
+    t = float(regex.findall(r"\{([^\}]+)\}",line)[0])
+    u = [float(val) for val in line.split()[1:]]
+    figure()
+    xlabel("x")
+    axis([0,1,0,1])
+    ylabel("u")
+    plot(x,u,label="t=%.2f" % t)
+    legend(loc="upper left")
+    savefig("%s%08d.png" % (name,i))
+    i += 1
+
+delay = int(round(1000.0/i))
+os.system("convert -delay %d -loop 0 %s*.png %s.gif" % (delay,name,name))
+os.system("rm *.png")
